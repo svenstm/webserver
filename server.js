3c92add3288484465827700275e1b27d5708509d
Sven Stam <sstam@ask-cs.com>
2012-10-17 11:36:28 +0200

    Made server multi threaded

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 var express = require('express'),
+	cluster = require('cluster'),
+	os = require('os'),
     app = express();
-    
 
 app.configure(function(){
+  /*app.use(function(req, res, next) {
+	console.log("["+process.pid+"] request on: "+req.url);
+	next();
+  });*/
   app.use(express.static(__dirname + '/public'));
   app.use(express.logger('dev'));
 });
 
 app.get('/', function(req, res){
-  res.sendfile(__dirname + '/public/index.html');
+  	res.sendfile(__dirname + '/public/index.html');
 });
 
 
@@ -16,4 +21,17 @@ app.use(function(req, res, next) {
 	res.sendfile(__dirname + req.url);
 });
 
-app.listen(3000);
+
+if (cluster.isMaster) {
+	for (var i = 0; i < os.cpus().length; i++) {
+		cluster.fork();
+	}
+	
+	cluster.on('listening', function(worker, address) {
+		console.log("Worker "+worker.process.pid+" listening on: "+address.port);
+	});
+	
+} else {
+
+	app.listen(3000);
+}
